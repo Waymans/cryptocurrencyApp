8bf22c87adc16e1d5a9c25bc74e8e612bb8ca03e
Waylan Hedine <hedin090@umn.edu>
2019-05-27 15:11:57 -0500

    added icons and modal display
    added icons, modal display, readme pictures, and icons file for requires

--- App.js
+++ App.js
@@ -6,9 +6,13 @@ import {
   ScrollView,
   Image,
   ImageBackground,
-  Platform,
+  Modal,
+  TouchableOpacity,
+  Alert,
 } from 'react-native';
+import { Constants } from 'expo';
 import { CRYPTO_KEY } from 'react-native-dotenv';
+import { icons } from './icons';
 
 export default class App extends React.Component {
   state = {
@@ -36,7 +40,6 @@ export default class App extends React.Component {
         loading: false,
         error: null,
       });
-      return null;
     } catch (error) {
       thus.setState({
         loading: false,
@@ -50,41 +53,14 @@ export default class App extends React.Component {
   renderError() {
     return <Text style={styles.center}>Uh oh: {this.state.error.message}</Text>;
   }
-  getColor(color) {
-    if (color < 0) {
-      return { color: 'red' };
-    } else {
-      return { color: 'green' };
-    }
-  }
   renderCryptos() {
     if (this.state.error) {
       return this.renderError();
     }
     return (
-      <View>
+      <View style={styles.main}>
         {this.state.crypto.map(list => (
-          <View style={styles.card}>
-            <View style={styles.contain}>
-              <Text>
-                <Text style={styles.symbol}>{list.symbol}</Text> |{' '}
-                <Text style={styles.name}>{list.name}</Text>
-              </Text>
-              <Text style={styles.price}>${list.quote.USD.price}</Text>
-              <Text>
-                24h:{' '}
-                <Text style={this.getColor(list.quote.USD.percent_change_24h)}>
-                  {list.quote.USD.percent_change_24h}%
-                </Text>
-              </Text>
-              <Text>
-                7d:{' '}
-                <Text style={this.getColor(list.quote.USD.percent_change_7d)}>
-                  {list.quote.USD.percent_change_7d}%
-                </Text>
-              </Text>
-            </View>
-          </View>
+          <CoinModal list={list} />
         ))}
       </View>
     );
@@ -107,11 +83,143 @@ export default class App extends React.Component {
   }
 }
 
+class CoinModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible: false,
+    };
+  }
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
+  render() {
+    const { list } = this.props;
+    return (
+      <View style={styles.container}>
+        <Modal
+          style={{backgroundColor: 'rgba(250,250,250,0.5)'}}
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={styles.modal}>
+            <View style={styles.containModal}>
+              {coinMaker(list)}
+              <TouchableOpacity
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text style={styles.close}>Close</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+        <View style={styles.card}>
+          <TouchableOpacity
+            onPress={() => {
+              this.setModalVisible(true);
+            }}>
+            <Image
+              style={styles.image}
+              source={validateIcons(list.symbol.toLowerCase())}
+            />
+          </TouchableOpacity>
+          {cardMaker(list)}
+        </View>
+      </View>
+    );
+  }
+}
+
+const cardMaker = list => {
+  return (
+    <View style={styles.cardSpread}>
+      <View style={styles.contain}>
+        <Text>
+          <Text style={styles.symbol}>{list.symbol}</Text> |{' '}
+          <Text>{list.name}</Text>
+        </Text>
+        <Text>Price: ${financial(list.quote.USD.price)}</Text>
+      </View>
+      <View style={styles.contain}>
+        <Text>
+          24h:{' '}
+          <Text style={getColor(list.quote.USD.percent_change_24h)}>
+            {financial(list.quote.USD.percent_change_24h)}%
+          </Text>
+        </Text>
+        <Text>
+          7d:{' '}
+          <Text style={getColor(list.quote.USD.percent_change_7d)}>
+            {financial(list.quote.USD.percent_change_7d)}%
+          </Text>
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const coinMaker = list => {
+  return (
+    <View style={styles.containModal}>
+      <Image
+        style={styles.imageModal}
+        source={validateIcons(list.symbol.toLowerCase())}
+      />
+      <Text style={styles.text}>
+        <Text style={styles.symbol}>{list.symbol}</Text> |{' '}
+        <Text style={styles.text}>{list.name}</Text>
+      </Text>
+      <Text style={styles.text}>Price: ${financial(list.quote.USD.price)}</Text>
+      <Text style={styles.text}>Rank: {list.cmc_rank}</Text>
+      <Text style={styles.text}>
+        1h:{' '}
+        <Text style={getColor(list.quote.USD.percent_change_1h)}>
+          {financial(list.quote.USD.percent_change_1h)}%
+        </Text>
+      </Text>
+      <Text style={styles.text}>
+        24h:{' '}
+        <Text style={getColor(list.quote.USD.percent_change_24h)}>
+          {financial(list.quote.USD.percent_change_24h)}%
+        </Text>
+      </Text>
+      <Text style={styles.text}>
+        7d:{' '}
+        <Text style={getColor(list.quote.USD.percent_change_7d)}>
+          {financial(list.quote.USD.percent_change_7d)}%
+        </Text>
+      </Text>
+      <Text style={styles.text}>
+        Added: {new Date(list.date_added).toDateString()}
+      </Text>
+    </View>
+  );
+};
+
+function validateIcons(icon) {
+  return icons[icon] ? icons[icon] : icons['coin'];
+}
+
+function getColor(color) {
+  if (color < 0) {
+    return { color: 'red' };
+  } else {
+    return { color: 'green' };
+  }
+}
+function financial(x) {
+  return Number.parseFloat(x).toFixed(2);
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    paddingTop: Platform.OS === 'ios' ? 20 : 5,
+    paddingTop: Constants.statusBarHeight,
     backgroundColor: 'transparent',
     padding: 8,
   },
@@ -122,16 +230,46 @@ const styles = StyleSheet.create({
   card: {
     backgroundColor: 'white',
     borderRadius: 5,
-    margin: 10,
+    margin: 5,
     padding: 5,
+    flexDirection: 'row',
   },
-  name: {},
-  symbol: { fontWeight: 'bold' },
-  price: {},
-  contain: {
+  cardSpread: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  image: {
+    width: 30,
+    height: 30,
+    marginTop: 2,
+    marginRight: 5,
+  },
+  imageModal: {
+    width: 30,
+    height: 30,
+    marginBottom: 30,
+  },
+  text: {
+    fontSize: 20,
+  },
+  symbol: { fontWeight: 'bold' },
+  contain: {
+    flexDirection: 'column',
     flexWrap: 'wrap',
+  },
+  modal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containModal: {
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  close: {
+    fontSize: 20,
   },
 });
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Fetches cryptocurrency data and displays it to the user.
 Uses React Native.
 
-<img src="./vid/cryptoAndroid.gif" width="250" height="400" />
+<img src="./display/cryp2.png" alt="Demo main display"/>
+<img src="./display/cryp1.png" alt="Demo modal display"/>
 
 Made with [Snack](https://github.com/expo/snack-web).
